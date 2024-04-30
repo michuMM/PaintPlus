e296c3dec0f3985fd06431c82cb5976b625270af
Kutabix <bartekd123456@wp.pl>
2024-04-30 15:05:03 +0200

    added drawing feature

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,75 @@
 #include "mainwindow.h"
-#include "ui_mainwindow.h"
+#include "./ui_mainwindow.h"
+#include<QDebug>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    qDebug()<<"Debug button";
+
+    QSize screenSize = QSize(833, 552);
+
+    image=QImage(screenSize,QImage::Format_RGB32);
+    image.fill(Qt::white);
+    drawing=false;
+    brushColor=Qt::blue;
+    brushsize=5;
+
+    setMouseTracking(true);
+    ui->centralwidget->setMouseTracking(true);
+
+    setWindowTitle(tr("PaintPlus"));
+
+    ui->statusBar->addPermanentWidget(ui->label);
+    ui->label->setText("Współrzędne kursora: ");
+
+    QWidget::setCursor(Qt::CrossCursor);
+
+    ui->statusBar->setStyleSheet("background-color: rgb(240,240,240);");
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::mousePressEvent(QMouseEvent *event)
+{
+    if(event->button()==Qt::LeftButton){
+        drawing=true;
+        lastPoint=event->pos();
+    }
+}
+
+void MainWindow::mouseMoveEvent(QMouseEvent *event)
+{
+    ui->label->setText("wspolrzednne: " + QString::number(event->pos().x()) + ", " + QString::number(event->pos().y()) + ")");
+
+    if((event->buttons()  & Qt::LeftButton) && drawing){
+        QPainter painter(&image);
+        painter.setPen(QPen(brushColor,brushsize,Qt::SolidLine,Qt::RoundCap,Qt::RoundJoin));
+        painter.drawLine(lastPoint,event->pos());
+        lastPoint=event->pos();
+        this->update();
+    }
+
+}
+
+void MainWindow::paintEvent(QPaintEvent *event)
+{
+    QPainter painter(this);
+    painter.drawImage(this->rect(),image,image.rect());
+}
+
+void MainWindow::mouseReleaseEvent(QMouseEvent *event)
+{
+    if(event->button()==Qt::LeftButton){
+        drawing=false;
+    }
+
+}
+
+
+
--- mainwindow.h
+++ mainwindow.h
@@ -2,11 +2,11 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-
+#include <QMouseEvent>
+#include <QPainter>
+#include <QFileDialog>
 QT_BEGIN_NAMESPACE
-namespace Ui {
-class MainWindow;
-}
+namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
@@ -16,8 +16,19 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+    virtual void mousePressEvent(QMouseEvent *event);
+    virtual void mouseMoveEvent(QMouseEvent *event);
+    virtual void mouseReleaseEvent(QMouseEvent *event);
+    virtual void paintEvent(QPaintEvent *event);
+
+private slots:
 
 private:
     Ui::MainWindow *ui;
+    QImage image;
+    bool drawing;
+    QPoint lastPoint;
+    int brushsize;
+    QColor brushColor;
 };
 #endif // MAINWINDOW_H
